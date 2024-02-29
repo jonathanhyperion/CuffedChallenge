133ef95e727a020d6b491587449d6f8b5d2d28e8
jonathanhyperion <jonathanhyperion@gmail.com>
2024-02-29 16:33:30 -0600

    :memo: cuffed challenge

---
+++ CuffedApp/Utils/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  CuffedApp
+//
+//  Created by Jonathan on 11/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum Constants {
+    // Colors
+    static let profileBackgoundColor = Color("profileBackgoundColor")
+    static let backgroundColorApp = Color("backgroundColorApp")
+    static let textColor = Color("Accent1")
+    static let addButtonColor = Color("Accent2")
+    static let accentPlaceHolderColor = Color("Accent3")
+    static let inputFieldColor = Color("Accent4")
+    static let overViewColor = Color("OverView")
+    // Numbers
+    static let zero = 0.0
+    static let one = 1.0
+    static let two = 2
+    static let four = 4
+}
